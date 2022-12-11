96c2d0cf4e2ae6b04a667bc6e082e484cb198a54
Florian Toque <florian.toque@gmail.com>
2022-12-12 00:15:48 +0100

    Ask mapbox Token and work in local fix

--- visualization/dialog_box.js
+++ visualization/dialog_box.js
@@ -2,14 +2,14 @@ var submitButton = document.getElementById('submit-button');
 
 // add a click event listener to the button
 submitButton.addEventListener('click', function() {
-  // get a reference to the dialog box element
-  var dialogBox = document.getElementById('dialog-box');
-  var mapboxApiKey=document.getElementById('mapbox_api_key').value;
-  loadMap(mapboxApiKey);
-  
-  // get a reference to the parent element of the dialog box
-  var parentElement = dialogBox.parentNode;
-
-  // remove the dialog box element from the parent element
-  parentElement.removeChild(dialogBox);
+    // get a reference to the dialog box element
+    var dialogBox = document.getElementById('dialog-box');
+    var mapboxApiKey=document.getElementById('mapbox_api_key').value;
+    loadMap(mapboxApiKey);
+    
+    // get a reference to the parent element of the dialog box
+    var parentElement = dialogBox.parentNode;
+    
+    // remove the dialog box element from the parent element
+    parentElement.removeChild(dialogBox);
 });
